88c226efb4949b3246f2aba83d7aecb6b1d0e2ef
AhmadDahlan <ahmddhln197@gmail.com>
2018-03-30 16:01:55 +0700

    Membuat atribut dan behavior

--- src/praktek01/baju.java
+++ src/praktek01/baju.java
@@ -10,5 +10,17 @@ package praktek01;
  * @author ASUS
  */
 public class baju {
-    
+   String merk;
+   String warna;
+   Double ukuran;
+   String bahan;
+   String corak;
+   
+   void pelindungtubuh(){
+       System.out.println("merk :"+merk);
+       System.out.println("warna :"+warna);
+       System.out.println("ukuran :"+ukuran);
+       System.out.println("bahan :"+bahan);
+       System.out.println("corak :"+corak);
+   }
 }
